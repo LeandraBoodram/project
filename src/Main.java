2741fcf11d018f40ef1cb9153ae26304662585c1
unknown <BT_4N2_02@BKTECH.LOCAL>
2023-09-19 12:46:12 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello my name is Leandra.");
+        System.out.println("My favorite movie is Avatar");
+
+        }
+    }
+}
